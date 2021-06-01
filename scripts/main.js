8dc51db78fe6cc08caba04d443c0783dc2722d49
Ethan Charles <59294780+ethancharles02@users.noreply.github.com>
2021-06-01 16:47:32 -0700

    Added jQuery inputs for focus, input, and blue Restricted input to only numbers Added full selection when user clicks input Added input list of values for summing

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,52 @@
-function add_dict_elements(selector, dict){
+// Add default values to json (if Shawn wants that)
+// Implement default values for new inputs
+// Add formatting for accounting values
+
+var monthly_overhead_inputs = {}
+
+function add_query_on_input(id, event, func){
+    jQuery("#" + id).on(event, func)
+}
+
+function format_value_to_currency(value){
+    return "$ " + parseFloat(value).toFixed(2)
+}
+
+function set_event_target_currency(event){
+    let value = event.target.value
+    if (jQuery.isNumeric(value)){
+        event.target.value = format_value_to_currency(event.target.value)
+    }
+}
+
+function restrict_input(event){
+    let value = event.target.value
+    let id = event.target.id
+
+    if (!jQuery.isNumeric(value) && value != ""){
+        event.target.value = monthly_overhead_inputs[id]
+    }
+    else{
+        if (value.includes(".")){
+            monthly_overhead_inputs[id] = parseFloat(value).toFixed(2)
+        }
+        else if(value == ""){
+            monthly_overhead_inputs[id] = value
+        }
+        else{
+            monthly_overhead_inputs[id] = parseFloat(value)
+        }
+    }
+}
+
+function focus_input(event){
+    event.target.value = monthly_overhead_inputs[event.target.id]
+    event.target.select()
+}
+
+function add_dict_elements(selector, dict, value_dict){
     for (const key in dict){
-        // console.log([key, dict[key]])
+        value_dict[key] = ""
         let div = document.createElement("div")
 
         let label = document.createElement("label")
@@ -8,10 +54,10 @@ function add_dict_elements(selector, dict){
         label.textContent = dict[key]["display_name"] + ":"
 
         let input = document.createElement("input")
-        input.type = "number"
+        input.type = "text"
         input.id = key
         input.name = key
-        
+
         div.appendChild(label)
         div.appendChild(input)
 
@@ -28,59 +74,18 @@ function add_dict_elements(selector, dict){
 
             div.appendChild(fixed_label)
             div.appendChild(fixed_input)
+            
         }
-
         document.querySelector(selector).appendChild(div)
+
+        add_query_on_input(key, "blur", set_event_target_currency)
+        add_query_on_input(key, "input", restrict_input)
+        add_query_on_input(key, "focus", focus_input)
     }
 }
-
-// document.querySelector("#inputs_monthly_overhead").appendChild(test_element)
 
 fetch("./data/monthly_overhead.json")
 .then(response => {
     return response.json()
 })
-.then(data => add_dict_elements("#inputs_monthly_overhead", data[0]))
-
-// function add(number1, number2){
-//     return number1 + number2
-// }
-
-// function addNumbers(){
-//     return document.querySelector("#sum").value = add(parseFloat(document.querySelector("#addend1").value), parseFloat(document.querySelector("#addend2").value))
-// }
-
-// document.querySelector("#addNumbers").addEventListener("click", addNumbers)
-
-// let subtract = function(number1, number2){
-//     return number1 - number2
-// }
-    
-// let subtractNumbers = function(){
-//     return document.querySelector("#difference").value = subtract(parseFloat(document.querySelector("#minuend").value), parseFloat(document.querySelector("#subtrahend").value))
-// }
-
-// document.querySelector("#subtractNumbers").addEventListener("click", subtractNumbers)
-
-// let multiply = (number1, number2) => number1 * number2
-
-// let multiplyNumbers = () => document.querySelector("#product").value = multiply(parseFloat(document.querySelector("#factor1").value), parseFloat(document.querySelector("#factor2").value))
-
-// document.querySelector("#multiplyNumbers").addEventListener("click", multiplyNumbers)
-
-// let divide = (number1, number2) => number1 / number2
-
-// let divideNumbers = () => document.querySelector("#quotient").value = divide(parseFloat(document.querySelector("#dividend").value), parseFloat(document.querySelector("#divisor").value))
-
-// document.querySelector("#divideNumbers").addEventListener("click", divideNumbers)
-
-
-
-
-// const date = new Date() 
-
-// let curYear
-
-// curYear = date.getFullYear()
-
-// document.querySelector("#year").textContent = curYear
+.then(data => add_dict_elements("#inputs_monthly_overhead", data[0], monthly_overhead_inputs))
