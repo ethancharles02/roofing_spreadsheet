c1c7b7fb7494144bad4c29864e8ae17ba78e5c45
Ethan Charles <59294780+ethancharles02@users.noreply.github.com>
2021-06-01 13:24:44 -0700

    Added Base HTML For Inputs Added Minimal CSS formatting

---
+++ scripts/main.js
@@ -0,0 +1,44 @@
+document.querySelector("#")
+
+// function add(number1, number2){
+//     return number1 + number2
+// }
+
+// function addNumbers(){
+//     return document.querySelector("#sum").value = add(parseFloat(document.querySelector("#addend1").value), parseFloat(document.querySelector("#addend2").value))
+// }
+
+// document.querySelector("#addNumbers").addEventListener("click", addNumbers)
+
+// let subtract = function(number1, number2){
+//     return number1 - number2
+// }
+    
+// let subtractNumbers = function(){
+//     return document.querySelector("#difference").value = subtract(parseFloat(document.querySelector("#minuend").value), parseFloat(document.querySelector("#subtrahend").value))
+// }
+
+// document.querySelector("#subtractNumbers").addEventListener("click", subtractNumbers)
+
+// let multiply = (number1, number2) => number1 * number2
+
+// let multiplyNumbers = () => document.querySelector("#product").value = multiply(parseFloat(document.querySelector("#factor1").value), parseFloat(document.querySelector("#factor2").value))
+
+// document.querySelector("#multiplyNumbers").addEventListener("click", multiplyNumbers)
+
+// let divide = (number1, number2) => number1 / number2
+
+// let divideNumbers = () => document.querySelector("#quotient").value = divide(parseFloat(document.querySelector("#dividend").value), parseFloat(document.querySelector("#divisor").value))
+
+// document.querySelector("#divideNumbers").addEventListener("click", divideNumbers)
+
+
+
+
+// const date = new Date() 
+
+// let curYear
+
+// curYear = date.getFullYear()
+
+// document.querySelector("#year").textContent = curYear
