c9d5230506190bb1e5ad30feaf7d3d1e32dc9295
ethancharles02 <59294780+ethancharles02@users.noreply.github.com>
2021-06-08 14:46:17 -0700

    Removed necessary json info for inputs/outputs

--- scripts/main.js
+++ scripts/main.js
@@ -8,6 +8,10 @@
 // Move the creation of the h2 element (section name) to only happen if there is a display name (same for others?)
 // Create a function for updating the output values, putting all of the formulas inside a json format wouldn't work easily
 // Merge the input and output name elements creation so that they are just created based on a display_name
+// Check for display name, if not there, use the variable name for display
+// Separate add_dict_elements into smaller functions
+// Fix inputs not getting formatted
+// Change default_fixed_value code to be based off a new very that say whether or not it can be either fixed or variable
 
 // func for getting information from a key inside a nested object
 // function getObject(theObject) {
@@ -150,72 +154,89 @@ function add_dict_elements(selector, dict, value_dict){
 
         // Runs through each of the section values (ex. display_name, inputs, outputs)
         for (const section_value in dict[section]){
-            if (section_value == "inputs"){
+            // console.log(section_value)
+            let section_value_header = document.createElement("h3")
+            section_value_header.textContent = dict[section][section_value]["display_name"]
+            document.querySelector(selector).appendChild(section_value_header)
 
-                let section_value_header = document.createElement("h3")
-                section_value_header.textContent = dict[section][section_value]["display_name"]
-                document.querySelector(selector).appendChild(section_value_header)
+            let section_value_article = document.createElement("article")
+            let article_id = section + "_" + section_value
+            section_value_article.id = article_id
+            document.querySelector(selector).appendChild(section_value_article)
 
-                let section_value_article = document.createElement("article")
-                let article_id = section + "_" + section_value
-                section_value_article.id = article_id
-                document.querySelector(selector).appendChild(section_value_article)
+            for (const key in dict[section][section_value]["display_values"]){
+                
+                let div = document.createElement("div")
 
-                for (const key in dict[section]["inputs"]["display_values"]){
+                let label = document.createElement("label")
+                label.for = key
+                label.textContent = dict[section][section_value]["display_values"][key]["display_name"] + ":"
+                div.appendChild(label)
+                
+                if (dict[section][section_value]["display_values"][key]["user_interaction"] == "input"){
                     value_dict[key] = ""
-                    let div = document.createElement("div")
-
-                    let label = document.createElement("label")
-                    label.for = key
-                    label.textContent = dict[section]["inputs"]["display_values"][key]["display_name"] + ":"
-
+                    
                     let input = document.createElement("input")
                     input.type = "text"
                     input.id = key
                     input.name = key
 
-                    div.appendChild(label)
+                    
                     div.appendChild(input)
 
-                    if ("default_fixed_value" in dict[section]["inputs"]["display_values"][key]){
+                    if ("default_fixed_value" in dict[section][section_value]["display_values"][key]){
                         let fixed_label = document.createElement("label")
                         fixed_label.for = key + "_fixed"
                         fixed_label.textContent = "Fixed:"
 
                         let fixed_input = document.createElement("input")
                         fixed_input.type = "checkbox"
-                        fixed_input.checked = dict[section]["inputs"]["display_values"][key]["default_fixed_value"]
+                        fixed_input.checked = dict[section][section_value]["display_values"][key]["default_fixed_value"]
                         fixed_input.id = key + "_fixed"
                         fixed_input.name = key
 
                         div.appendChild(fixed_label)
                         div.appendChild(fixed_input)
-                        
                     }
-                    document.querySelector("#" + article_id).appendChild(div)
-
+                    
                     add_query_on_input(key, "blur", set_event_target_currency)
+                    add_query_on_input(key, "blur", update_outputs)
                     add_query_on_input(key, "input", restrict_input)
                     add_query_on_input(key, "focus", focus_input)
                 }
-            }
-            if (section_value == "outputs"){
-                let section_value_header = document.createElement("h3")
-                section_value_header.textContent = dict[section][section_value]["display_name"]
-                document.querySelector(selector).appendChild(section_value_header)
 
-                let section_value_article = document.createElement("article")
-                let article_id = section + "_" + section_value
-                section_value_article.id = article_id
-                document.querySelector(selector).appendChild(section_value_article)
+                else if (dict[section][section_value]["display_values"][key]["user_interaction"] == "output"){
+                    output_label = document.createElement("label")
+                    output_label.id = key + "_value"
+
+                    div.appendChild(output_label)
+                }
+
+                document.querySelector("#" + article_id).appendChild(div)
             }
         }
     }
 }
 
-// Gets the json file and converts it to an array which is used as an argument in the add_dict_elements function
-fetch("./data/data_sheet_variables.json")
-.then(response => {
-    return response.json()
-})
-.then(data => add_dict_elements("#data_sheet_variables", data[0], data_sheet_input_values))
+function update_outputs(){
+    update_monthly_overhead_outputs()
+}
+
+function update_monthly_overhead_outputs(){
+    console.log(data_sheet_input_values)
+    // document.querySelector("#est_monthly_expenses_fixed_value").textContent = data_sheet_input_values["monthly_overhead"]
+}
+
+function main(){
+    // Gets the json file and converts it to an array which is used as an argument in the add_dict_elements function
+    fetch("./data/data_sheet_variables.json")
+    .then(response => {
+        return response.json()
+    })
+    .then(data => {
+        add_dict_elements("#data_sheet_variables", data[0], data_sheet_input_values)
+        update_outputs()
+    })
+}
+
+main()
